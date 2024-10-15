f862391fa833affdd1a8910643f263409803ae96
Abdullah021Siraj <work.abdullah021@gmail.com>
2024-10-15 13:01:03 +0500

    Revert "Reset Password Form"
    This reverts commit a20157dcef1631a360277b0d99e1e1c2ccd09980.

--- src/routes.js
+++ src/routes.js
@@ -11,8 +11,7 @@ export const authRoutes = [
     '/auth/login',
     '/auth/register',
     '/auth/error',
-    '/auth/reset',
-    '/auth/new-password'
+    '/auth/reset'
 ]
 
 //routes for API authentication purposes
